fbaa105c8562aad89551390f6ce2f14ff6a43f42
Sokhibjon18 <safarovsoxibjon18@gmail.com>
2022-06-13 15:37:34 +0500

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
