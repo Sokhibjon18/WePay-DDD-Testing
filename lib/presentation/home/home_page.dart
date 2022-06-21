14fc1d038e22a081300f38ef0b7eaa24d486ce40
jushkinParmonov <jushkinparmonoviut@gmail.com>
2022-06-21 12:57:43 +0500

    bottomsheet added

--- lib/presentation/home/home_page.dart
+++ lib/presentation/home/home_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'reusables.dart';
 
-import 'adding_page.dart';
 
 class Home_page extends StatefulWidget {
   const Home_page({Key? key}) : super(key: key);
@@ -11,8 +10,6 @@ class Home_page extends StatefulWidget {
 }
 
 class _Home_pageState extends State<Home_page> {
-  List<Data> information = [];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,37 +18,10 @@ class _Home_pageState extends State<Home_page> {
         automaticallyImplyLeading: false,
         actions: [
           IconButton(
-            onPressed: () async {
-              Data data = await Navigator.push(
-                  context, MaterialPageRoute(builder: (_) => Adding()));
-              setState(() => information.add(data));
+            onPressed: () {
+              bottomsheet(context);
             },
             icon: const Icon(Icons.add),
-          ),
-        ],
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Expanded(
-            child: ListView.builder(
-                shrinkWrap: true,
-                itemCount: information.length,
-                itemBuilder: (context, index) {
-                  return Dismissible(
-                    key: const ValueKey('Dismiss'),
-                    background: Container(
-                      color: Colors.red,
-                      child: const Icon(Icons.delete, color: Colors.white,),
-                    ),
-                    child: taskContainer(information[index], context),
-                    onDismissed: (direction){
-                      setState(() {
-                        information.removeAt(index);
-                      });
-                    },
-                  );
-                }),
           ),
         ],
       ),
@@ -59,14 +29,3 @@ class _Home_pageState extends State<Home_page> {
   }
 }
 
-class Data {
-  late String location;
-  late String people;
-  late String price;
-
-  Data(
-      this.location,
-      this.people,
-      this.price,
-      );
-}
