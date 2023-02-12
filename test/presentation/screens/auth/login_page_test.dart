58a44b632b8e532749a0ac5ec106413f9173eb9b
Sokhibjon18 <safarovsoxibjon18@gmail.com>
2023-02-12 10:46:22 +0500

    unit and bloc testing

---
+++ test/presentation/screens/auth/login_page_test.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:we_pay/presentation/screens/auth/login_page.dart';
+
+void main() {
+  // testWidgets(
+  //   'Testing if there is LogIn Form',
+  //   (WidgetTester tester) async {
+  //     await tester.pumpWidget(LogInPage());
+  //     final CommonFinders finder = find.text('Salom, hush kelibsiz') as CommonFinders;
+  //     expect(finder, findsWidgets);
+  //   },
+  // );
+}
